ac96a5adc3eae62b1b428c4ca403d1dfce3fdbda
铁拳无敌俞大猷 <lishiwen@qq.com>
2019-11-28 17:46:41 +0800

    试验粒子效果试验类的参数传递

--- NameWarIII/Assets/Scenes/Test.cs
+++ NameWarIII/Assets/Scenes/Test.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Test : MonoBehaviour
-{ 
-    int a;
-    bool isOk;
+{
+    Test A;
+    Test B;
+
+    string name;
 
     private void Start()
     {
-
+        A = new Test();
+        B = new Test();
+        A.name = "A";
+        B.name = "B";
+        Experiment(A);
+        Experiment(ref A);
+        
     }
 
-    void OnParticleSystemStopped()
+    void Experiment(Test exp)
     {
-        Debug.Log("试试");
+        exp = B;
+        Debug.Log("没有Ref exp=B后, exp.name是" + (exp.name) + "||A.name是" + A.name + "||A这个符号代表的实例的地址值没变化");    
+    }
+
+    void Experiment(ref Test exp)
+    {
+        exp = B;
+        Debug.Log("但是加了有Ref exp=B后, exp.name是" + (exp.name) + "||A.name是" + A.name + "||A这个符号代表的实例的地址值已经变成了B的地址值");
     }
 
 }
